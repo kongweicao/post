76e3e7c305c98c305c3418f2d2fb08a7f23757df
Kong Mike <weicao8@126.com>
2021-10-11 17:01:32 +0800

    代码提交

--- src/main/java/com/example/springbootdemo/service/DataService.java
+++ src/main/java/com/example/springbootdemo/service/DataService.java
@@ -9,6 +9,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,10 +29,21 @@ public class DataService {
       postResult = postResult.substring(postResult.indexOf("{"), postResult.lastIndexOf("}")+1);
       JSONObject postObject = JSONUtil.parseObj(postResult);
       String content = postObject.getStr("content");
-      if(!StringUtil.isBlank(content)){
+      if(!StringUtil.isBlank(content)) {
         Document document = Jsoup.parse(content);
-        Element element = document.getElementsByAttribute("href='http://fund.eastmoney.com/000083.html'").first();
-        System.out.println("--------begin:" + element + "----------end");
+        Elements elements = document.getElementsByTag("a");
+        int i = 0;
+        for (Element element : elements) {
+          if (i == 0) {
+            log.info("---------fund name is:{}---------", element.html());
+          }
+          log.info("---------i:{}---------", i);
+          if ((i - 2) % 6 == 0) {
+            log.info("---------post name is:{}---------", element.html());
+          }
+          i++;
+        }
+        System.out.println("--------begin:" + elements + "----------end");
       }
     }
     //log.info(JSONUtil.toJsonStr(fundArray));
